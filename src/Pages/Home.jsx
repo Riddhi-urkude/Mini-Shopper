23faa76d9494adefc969af5627c0355e6a23f2b3
Riddhi-urkude <riddhi.gajananurkude2@cognizant.com>
2024-05-24 10:29:20 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <Hero
       title={"SHOPPER"}
       description={
-        "Welcome to SHOPPER, your one-stop-shop for the latest electronics. Browse our selection of smartphones, laptops, smart home devices, and more. Our easy-to-use platform makes it simple to find and purchase the products you need. Plus, enjoy exceptional customer service and support from our dedicated team. Discover the QuickPik difference and start shopping for your favorite electronics today!"
+        "Welcome to SHOPPER, your one-stop-shop for the latest electronics. Browse our selection of smartphones, laptops, smart home devices, and more. Our easy-to-use platform makes it simple to find and purchase the products you need. Plus, enjoy exceptional customer service and support from our dedicated team."
       }
     >
     </Hero>
