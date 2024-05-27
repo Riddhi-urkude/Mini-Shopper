b650a0c34306150d97d5bf40fc21437e26ef0085
Riddhi-urkude <riddhi.gajananurkude2@cognizant.com>
2024-05-27 15:00:56 +0530

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -82,7 +82,7 @@ export const Register = () => {
               />
               <div className="d-flex flex-column justify-content-center">
                 <h4 className="m-0" style={{ fontSize: "1rem" }}>
-                  QuickPik
+                  MINI-SHOPPER
                 </h4>
                 <small style={{ fontSize: "0.8rem" }}>
                   Rapid Reflection, Swift Selection
