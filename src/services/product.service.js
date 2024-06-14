91034716aa2047c1c7bf0f908285b41a681714b6
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-14 12:19:44 +0530

    Updated product.service.js

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,142 +1,29 @@
-import { API_ENDPOINTS } from "./helper.service";
-import { privateAxios, publicAxios } from "./axios.service";
+import axios from "axios";
 
-// get products
-export const getProducts = (
-  pageNumber,
-  pageSize = API_ENDPOINTS.PRODUCT_PAGE_SIZE,
-  sortBy = "createdAt",
-  sortDir = "asc"
-) => {
-  return publicAxios
-    .get(
-      `${API_ENDPOINTS.PRODUCTS}?pageNumber=${pageNumber}&pageSize=${pageSize}&sortBy=${sortBy}&sortDir=${sortDir}`
-    )
-    .then((res) => {
-      return res.data;
-    });
-};
+export const getAllProducts= async (page) =>{
+      try {
+        return await axios.get(`http://localhost:8080/products/listAllProducts`);
+//          return await axios.post(`http://localhost:8080/products/productCategory/${category}`);
+//          return await axios.post(`http://localhost:8080/products/updateProduct`);
+       } catch (err) {
+         alert(err);
+       }
+}
 
-// get products live
-export const getProductsLive = async (
-  pageNumber,
-  pageSize = API_ENDPOINTS.ALL_PRODUCT_PAGE_SIZE,
-  sortBy = "createdAt",
-  sortDir = "asc"
-) => {
-  const result = await publicAxios.get(
-    `${API_ENDPOINTS.PRODUCTS}/live?pageNumber=${pageNumber}&pageSize=${pageSize}&sortBy=${sortBy}&sortDir=${sortDir}`
-  );
-  return result.data;
-};
-
-// get products by id
-export const getProductById = async (productId) => {
-  const result = await publicAxios.get(
-    API_ENDPOINTS.PRODUCTS + "/" + productId
-  );
-  return result.data;
-};
-
-// get products by category id
-export const getProductsByCategoryId = async (
-  categoryId,
-  pageNumber,
-  pageSize = API_ENDPOINTS.PRODUCT_PAGE_SIZE,
-  sortBy = "createdAt",
-  sortDir = "asc"
-) => {
-  const result = await publicAxios.get(
-    `${API_ENDPOINTS.CATEGORIES}/${categoryId}${API_ENDPOINTS.PRODUCTS}?pageNumber=${pageNumber}&pageSize=${pageSize}&sortBy=${sortBy}&sortDir=${sortDir}`
-  );
-  return result.data;
-};
-
-// create a new product without category
-export const addProductWithoutCategory = (product) => {
-  return privateAxios.post(API_ENDPOINTS.PRODUCTS, product).then((res) => {
-    return res.data;
-  });
-};
-
-// create a new product with category
-export const addProductWithCategory = (product, categoryId) => {
-  return privateAxios
-    .post(
-      `${API_ENDPOINTS.CATEGORIES}/${categoryId}${API_ENDPOINTS.PRODUCTS}`,
-      product
-    )
-    .then((res) => {
-      return res.data;
-    });
-};
-
-// update product
-export const updateProduct = (product, productId) => {
-  return privateAxios
-    .put(API_ENDPOINTS.PRODUCTS + "/" + productId, product)
-    .then((res) => {
-      return res.data;
-    });
-};
-
-// get product image
-export const getImageByProductId = (productId) => {
-  return publicAxios
-    .get(API_ENDPOINTS.PRODUCT_IMAGES + "/" + productId, {
-      responseType: "arraybuffer", // Set the responseType to 'arraybuffer'
-    })
-    .then((res) => {
-      return res.data;
-    });
-};
-
-// upload product image
-export const uploadProductImage = (productImage, productId) => {
-  if (productImage == null) {
-    return;
+export const getProductById = async (productId) =>{
+  try {
+    const result = await axios.post(`http://localhost:8080/products/productId/${productId}`); 
+     return result.data;
+  } catch (err) {
+    alert(err);
   }
+}
 
-  const data = new FormData();
-  data.append("image", productImage);
+export const getProductsByCategoryId = async (categoryId,page) =>{
+   return  await axios.get(`http://localhost:8080/categories/${categoryId}/products`);
+ // console.log(res.data);
 
-  return privateAxios
-    .post(API_ENDPOINTS.PRODUCT_IMAGES + "/" + productId, data)
-    .then((res) => {
-      return res.data;
-    });
-};
 
-// update product details
-export const updateProductDetails = (product, productId) => {
-  return privateAxios
-    .put(API_ENDPOINTS.PRODUCTS + "/" + productId, product)
-    .then((res) => {
-      return res.data;
-    });
-};
 
-// update category of product
-export const updateProductCategory = (categoryId, productId) => {
-  return privateAxios
-    .put(
-      API_ENDPOINTS.CATEGORIES +
-        "/" +
-        categoryId +
-        API_ENDPOINTS.PRODUCTS +
-        "/" +
-        productId
-    )
-    .then((res) => {
-      return res.data;
-    });
-};
+}
 
-// search products by product name
-export const searchProducts = (productName) => {
-  return publicAxios
-    .get(API_ENDPOINTS.PRODUCTS + "/search/" + productName)
-    .then((res) => {
-      return res.data;
-    });
-};
