fd47c752fdb088ff8ec98ec4979261fbf2ba9117
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-14 12:18:44 +0530

    Updated categories.service.js

--- src/services/categories.service.js
+++ src/services/categories.service.js
@@ -1,58 +1,17 @@
-import { privateAxios, publicAxios } from "./axios.service";
-import { API_ENDPOINTS } from "./helper.service";
+import axios from "axios";
 
-export const getCategories = (
-  currentPage = 0,
-  pageSize = API_ENDPOINTS.CATEGORY_PAGE_SIZE
-) => {
-  return publicAxios
-    .get(
-      `${API_ENDPOINTS.CATEGORIES}?pageNumber=${currentPage}&pageSize=${pageSize}`
-    )
-    .then((res) => {
-      return res.data;
-    });
-};
-
-export const getCategoryById = async (categoryId) => {
-  const result = await publicAxios.get(API_ENDPOINTS.CATEGORIES + "/" + categoryId);
-  return result.data;
+export const getAllCategories = async ()=>{
+ // const res= 
+  return axios.get(`http://localhost:8080/categories`);
+ // return res.data;
 }
 
-export const addCategory = (data) => {
-  return privateAxios.post(API_ENDPOINTS.CATEGORIES, data).then((res) => {
+
+export const getCategoryById = async (categoryId)=>{
+    const res = await axios.get(`http://localhost:8080/categories/${categoryId}`);
     return res.data;
-  });
-};
 
-export const updateCategory = (categoryId, data) => {
-  return privateAxios
-    .put(API_ENDPOINTS.CATEGORIES + "/" + categoryId, data)
-    .then((res) => {
-      return res.data;
-    });
-};
 
-export const deleteCategory = (categoryId) => {
-  return privateAxios
-    .delete(API_ENDPOINTS.CATEGORIES + "/" + categoryId)
-    .then((res) => {
-      return res.data;
-    });
-};
+  
+}
 
-// upload image for category
-export const uploadCategoryImage = (categoryImage, categoryId) => {
-  if (categoryImage == null) {
-    return;
-  }
-
-  const data = new FormData();
-  data.append("image", categoryImage);
-
-  return privateAxios
-    .post(API_ENDPOINTS.CATEGORIES_IMAGES + "/" + categoryId, data)
-    .then((res) => {
-      return res.data;
-    });
-};
