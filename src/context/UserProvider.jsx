8a65d401f627846c6e71818537de136f034234fb
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-14 12:07:07 +0530

    Updated UserProvider.jsx

--- src/context/UserProvider.jsx
+++ src/context/UserProvider.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import { useEffect } from "react";
+import { UserContext } from "./UserContext";
+import { useState } from "react";
+import { doLoginLocalStorage, doLogoutLocalStorage, getUserFromLocalStorage, isLoggedIn } from "../auth/HelperAuth";
 
-const UserProvider = () => {
+const UserProvider = ({ children }) => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userData, setUserData] = useState(null);
+
+  useEffect(()=>{
+    // set login value and user data from local storage (check in auth/helper.auth.js)
+    setIsLogin(isLoggedIn);
+    setUserData(getUserFromLocalStorage);
+  },[])
+
+  // login
+  const doLogin = (user) => {
+    // set user data and token in local storage
+    doLoginLocalStorage(user);
+
+    // set login value and user data in user context
+    setIsLogin(true);
+    setUserData(getUserFromLocalStorage)
+  }
+
+  // logout
+  const doLogout = () => {
+    // remove user data and token from local storage
+    doLogoutLocalStorage();
+
+    // remove login value and user data in user context
+    setIsLogin(false);
+    setUserData(null);
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <UserContext.Provider
+      value={{
+        userData: userData,
+        isLogin: isLogin,
+        doLogin: doLogin,
+        doLogout: doLogout
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
 
-export default UserProvider
+export default UserProvider;
