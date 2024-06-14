ad0beafd7b638bd23e56f27aeeead7335baa14ad
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-14 12:16:26 +0530

    Updated HelperAuth.js

--- src/auth/HelperAuth.js
+++ src/auth/HelperAuth.js
@@ -1,9 +1,9 @@
 import { ROLES } from "../utils/roles";
 
 // store user data and token in local storage
-export const doLoginLocalStorage = (user, token) => {
+export const doLoginLocalStorage = (user) => {
   localStorage.setItem("userData", JSON.stringify(user));
-  localStorage.setItem("token", JSON.stringify(token));
+ // localStorage.setItem("token", JSON.stringify(token));
 };
 
 // get user data from local storage
@@ -45,19 +45,8 @@ export const isLoggedIn = () => {
   }
 };
 
-export const isAdminUser = () => {
-  if (isLoggedIn) {
-    const roles = getUserFromLocalStorage()?.roles;
-    if (roles.find((role) => role.roleName === ROLES.ADMIN)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
-
 // remove user data and token from local storage
 export const doLogoutLocalStorage = () => {
   localStorage.removeItem("userData");
-  localStorage.removeItem("token");
+ // localStorage.removeItem("token");
 };
