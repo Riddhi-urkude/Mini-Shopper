0cb9f57038c10e4a6cf8e778c33341b06a3db3f7
Riddhi-urkude <riddhi.gajananurkude2@cognizant.com>
2024-06-19 16:02:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,33 +6,41 @@ import Home from "./Pages/Home";
 import { About } from "./Pages/About";
 import { useState } from "react";
 import Contact from "./Pages/Contact";
-
+ 
 import { CategorySideBar } from "./Components/CategorySideBar";
-import {CategoryProductsPage} from "./Pages/users/CategoryProductsPage";
-
+import { CategoryProductsPage } from "./Pages/Users/CategoryProductsPage";
+ 
 import UserProvider from "./context/UserProvider";
 import { CategoryProvider } from "./context/CategoryProvider";
-import {Products} from "./Pages/users/Products";
-import {SingleProductPage} from "./Pages/users/SingleProductPage";
-
-import Profile from "./Pages/users/Profile";
-import { ShoppingCart } from "./Pages/users/ShoppingCart";
+import { Products } from "./Pages/Users/Products";
+import { SingleProductPage } from "./Pages/Users/SingleProductPage";
+ 
+import Profile from "./Pages/Users/Profile";
+import { ShoppingCart } from "./Pages/Users/ShoppingCart";
 import { CartProvider } from "./context/CartProvider";
-import {OrderCheckout} from "./Pages/users/OrderCheckout";
-import { Orders } from "./Pages/users/Orders";
-import { OrderDetail } from "./Pages/users/OrderDetail";
+import { OrderCheckout } from "./Pages/Users/OrderCheckout"
+import { Orders } from "./Pages/Users/Orders";
+import { OrderDetail } from "./Pages/Users/OrderDetail"
+import { UploadExcelSheet } from "./Pages/Users/UploadExcelSheet"
 
 import PrivateRoutes from "./Components/auth/PrivateRoutes";
-
+ 
 const App = () => {
   // state for category sidebar
   const [showCategorySidebar, setShowCategorySidebar] = useState(false);
-
+ 
   // functions for category sidebar
   const handleCloseCategorySidebar = () => setShowCategorySidebar(false);
   const handleShowCategorySidebar = () => setShowCategorySidebar(true);
 
+  const [orders, SetOrders] = useState([]);
 
+  // const handleAddOrder = (newOrders) => {
+  //   SetOrders([...orders, newOrder]);
+  //   // Handle saving order to backend or local storage as needed
+  // };
+ 
+ 
   return (
     <div>
       <UserProvider>
@@ -54,17 +62,17 @@ const App = () => {
               <Route path="/about" element={<About />}></Route>
               <Route path="/contact" element={<Contact />}></Route>
               <Route path="/products" element={<Products />}></Route>
-
+ 
               <Route
                 path="/product/:productId"
                 element={<SingleProductPage />}
               ></Route>
-            
+           
              <Route
                 path="/category/:categoryId/products"
                 element={<CategoryProductsPage />}
               ></Route>
-
+ 
               <Route
                 element={
                   <PrivateRoutes  />
@@ -73,9 +81,10 @@ const App = () => {
                  <Route path="/profile" element={<Profile/>}></Route>
                <Route path="/cart" element={<ShoppingCart />}></Route>
                <Route path="/place-order" element={<OrderCheckout />}></Route>
+               <Route path="/UploadExcelSheet" element={<UploadExcelSheet />}></Route>
                <Route path="/orders" element={<Orders />}></Route>
                <Route path="/order/:orderId" element={<OrderDetail />}></Route>
-               </Route> 
+               </Route>
               {/* Routes only admin and logged in user can access*/}
                {/* <Route
                 element={
@@ -84,14 +93,14 @@ const App = () => {
               >
                 <Route path="/profile" element={<Profile />}></Route>
                </Route>  */}
-              </Routes> 
+              </Routes>
         {/* </AuthProvider> */}
         </CategoryProvider>
         </CartProvider>
         </UserProvider>
     </div>
-    
+   
   );
 };
-
+ 
 export default App;
