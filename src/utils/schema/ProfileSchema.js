d18efd647588c25bfd8027779d6d73289e46f0b4
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-21 13:10:21 +0530

    added for profile

---
+++ src/utils/schema/ProfileSchema.js
@@ -0,0 +1,28 @@
+import * as Yup from "yup";
+import { REGEX_VALIDATIONS } from "../regex";
+ 
+export const profileSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(1, "First Name need atleast 1 characters")
+    .required("Please provide a first name"),
+  lastName: Yup.string()
+    .min(2, "Last Name need atleast 2 characters")
+    .required("Please provide a last name"),
+  state: Yup.string()
+    .required(
+      "State is required"
+    ),
+  address: Yup.string()
+  .min(10, "Address should be atleast 100 characters")
+  .required("Address is required"),
+ 
+  pinCode: Yup.string()
+  .min(6, "Pincode should contain 6 Numbers")
+  .max(6, "Pincode should not contain more than 6 Numbers")
+  .matches(REGEX_VALIDATIONS.POSTAL_CODE,
+    "Pincode is required"),
+ 
+  city: Yup.string().required("City is required"),
+ 
+  street: Yup.string().required("Street is required"),
+});
