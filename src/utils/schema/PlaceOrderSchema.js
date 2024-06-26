49d648dbedf9db55c5b14ea50faf36ccb354854f
Riddhi-urkude <riddhi.gajananurkude2@cognizant.com>
2024-06-26 15:25:35 +0530

    Update PlaceOrderSchema.js

--- src/utils/schema/PlaceOrderSchema.js
+++ src/utils/schema/PlaceOrderSchema.js
@@ -2,21 +2,22 @@ import * as Yup from "yup";
 import { REGEX_VALIDATIONS } from "../regex";
 
 export const placeOrderSchema = Yup.object().shape({
-  orderName: Yup.string()
-    .min(1, "Name need atleast 1 character")
-    .required("Please provide a name"),
-  shippingAddress: Yup.string().required("Please provide a shipping adress"),
-  postalCode: Yup.string()
+  firstName: Yup.string().required("Please provide a first name"),
+  lastName: Yup.string().required("Please provide a last name"),
+  
+    shippingAddress: Yup.string()
+  .min(10, "Address should be atleast 100 characters")
+  .required("Address is required"),
+  
+  pinCode: Yup.string()
   .min(6, "Pincode should contain 6 Numbers")
   .max(6, "Pincode should not contain more than 6 Numbers")
-    .matches(
-      REGEX_VALIDATIONS.POSTAL_CODE,
-      "Please provide a valid postal code"
-    )
-    .required("Please provide a postal code"),
+  .matches(REGEX_VALIDATIONS.POSTAL_CODE,
+    "Pincode is required"),
+    
   city: Yup.string().required("Please provide a city"),
   state: Yup.string().required("Please provide a sate"),
-  shippingPhone: Yup.string()
+  phoneNumber: Yup.string()
   .min(10, "Shipping Phone should contain 10 Numbers")
   .max(10, "Shipping Phone should not contain more than 10 Numbers")
     .matches(
