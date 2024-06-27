84543d1f944e3a5e2b78fff5b4eab43d7714bcf9
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-27 12:39:28 +0530

    Updated LoginSchema.js after completing US_04

--- src/utils/schema/LoginSchema.js
+++ src/utils/schema/LoginSchema.js
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
 export const loginSchema = Yup.object().shape({
-    userId: Yup.string().required("Please provide an email/userId"),
+    userId: Yup.string().required("Please provide an email"),
     password: Yup.string().required("Please provide a password")
 });
