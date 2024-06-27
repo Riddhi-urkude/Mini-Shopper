2ff85e7357974c04d03726c7522097532f578eb9
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-27 12:11:46 +0530

    Updated ProductCard.jsx after completing US_04

--- src/Components/users/ProductCard.jsx
+++ src/Components/users/ProductCard.jsx
@@ -1,16 +1,17 @@
 import React, { useContext } from "react";
 import { Badge, Button, Card, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { CartContext } from "../../context/CartContext";
+import { CartContext } from "../../Context/CartContext";
 import { toast } from "react-toastify";
-import { UserContext } from "../../context/UserContext";
+import { UserContext } from "../../Context/UserContext";
 
 export const ProductCard = ({ product }) => {
   const navigate = useNavigate();
   const { addItem } = useContext(CartContext);
   const { isLogin } = useContext(UserContext);
 
-  const handleAddToCart = (productId, quantity = 1) => {
+
+   const handleAddToCart = (productId, quantity = 1) => {
     if (isLogin) {
       const data = {
         productId,
@@ -34,10 +35,10 @@ export const ProductCard = ({ product }) => {
     <Col className="mb-3" md={6} lg={4} xl={3}>
       <Card>
         <img
-          src={`${process.env.REACT_APP_IMAGE_KIT_URL}/tr:h-300,w-400/products/${product.productImage}`}
+          src={'data:image/jpeg;base64,' +product.image}   
           alt={product.productName}
           width="100%"
-          height="180px"
+          height="250px"
           onClick={() => navigate(`/product/${product.productId}`)}
           style={{
             objectFit: "cover",
