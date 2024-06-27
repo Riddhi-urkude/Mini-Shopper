1f2ade6a667d0355b0bc4cab45177bbc491e06a8
Balaji-2180 <balaji.a8@cognizant.com>
2024-06-27 12:35:24 +0530

    Updated axios.service.js after completing US_04

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { API_ENDPOINTS } from "./helper.service";
+import { API_ENDPOINTS } from "./Helper.Service";
 import {
   doLogoutLocalStorage,
   getTokenFromLocalStorage,
@@ -8,38 +8,64 @@ import {
 import { toast } from "react-toastify";
 
 // Create a new axios instance with a baseURL that will be used for all requests
-export const publicAxios = axios.create({
-  baseURL: API_ENDPOINTS.BASE_URL,
+
+//const {token} = getTokenFromLocalStorage();
+//const token="csdc";
+
+export const publicAxiosInstance = axios.create({
+  baseURL: `http://localhost:8080`,
+    // headers: {
+    //   "Authorization" : `Bearer ${token.accessToken}`,
+    // }
+
 });
 
-// Private Axios
-export const privateAxios = axios.create({
-  baseURL: API_ENDPOINTS.BASE_URL,
-});
+let {token} = {accessToken:'', refreshToken: ''};
 
-const refreshToken = async () => {
-  const { refreshToken } = getTokenFromLocalStorage();
-  const result = await axios.post(
-    `${API_ENDPOINTS.BASE_URL}/auth/refresh`,
-    {},
-    {
-      headers: {
-        Authorization: `Bearer ${refreshToken}`,
-      },
-    }
-  );
-  return result.data.accessToken;
-};
+function getAccesToken(){
+  if(getTokenFromLocalStorage()!==null){
+    token=getTokenFromLocalStorage();
+  }
+  return token.accessToken;
+}
+
+export const privateAxiosInstance = axios.create({
+    baseURL: `http://localhost:8080`,
+    //   headers: {
+    //     "Authorization" : `Bearer ${getAccesToken()}`,
+    //   }
+  
+  });
+
+// const refreshToken = async () => {
+//   const { refreshToken } = getTokenFromLocalStorage();
+//   const result = await axios.post(
+//     `http://localhost:8080/users/refresh`,
+//     {},
+//     {
+//       headers: {
+//         Authorization: `Bearer ${refreshToken}`,
+//       },
+//     }
+//   );
+//   return result.data.accessToken;
+// };
 
 // Add a request interceptor to the privateAxios instance
-privateAxios.interceptors.request.use(
+privateAxiosInstance.interceptors.request.use(
   (config) => {
     // Get the token from local storage
     const tokens = getTokenFromLocalStorage();
+//    console.log("in use private instance");
+   // console.log(tokens);
     if (tokens !== null) {
       // Set the Authorization header for the request
-      config.headers.common.Authorization = `Bearer ${tokens.accessToken}`;
+      //console.log("inside if ");
+      //console.log(tokens.accessToken);
+      config.headers.Authorization = `Bearer ${tokens.accessToken}`;
     }
+    //console.log(config);
+   // console.log("end in ");
     return config;
   },
   (error) => {
@@ -49,45 +75,45 @@ privateAxios.interceptors.request.use(
 );
 
 // Add a response interceptor to the privateAxios instance
-privateAxios.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
+// privateAxiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    // Check if the error response has a status code of 401 (unauthorized)
-    if (
-      error.response &&
-      error.response.status === 401 &&
-      !originalRequest._retry
-    ) {
-      originalRequest._retry = true;
+//     // Check if the error response has a status code of 401 (unauthorized)
+//     if (
+//       error.response &&
+//       error.response.status === 401 &&
+//       !originalRequest._retry
+//     ) {
+//       originalRequest._retry = true;
 
-      try {
-        // Refresh the access token
-        const newAccessToken = await refreshToken();
+//       try {
+//         // Refresh the access token
+//         const newAccessToken = await refreshToken();
 
-        // Update the access token in local storage
-        updateAccessTokenInLocalStorage(newAccessToken);
+//         // Update the access token in local storage
+//         updateAccessTokenInLocalStorage(newAccessToken);
 
-        // Update the Authorization header with the new access token
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+//         // Update the Authorization header with the new access token
+//         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
 
-        // Retry the original request
-        return privateAxios(originalRequest);
-      } catch (refreshError) {
-        // Clear the tokens in local storage
-        doLogoutLocalStorage();
+//         // Retry the original request
+//         return privateAxiosInstance(originalRequest);
+//       } catch (refreshError) {
+//         // Clear the tokens in local storage
+//         doLogoutLocalStorage();
 
-        // show a msg 
-        toast.info("Your session has expired! please login again", {
-          autoClose: false,
-        });
+//         // show a msg 
+//         toast.info("Your session has expired! please login again", {
+//           autoClose: false,
+//         });
         
-        // redirect to login page
-        window.location.href = "/login";
-      }
-    }
+//         // redirect to login page
+//         window.location.href = "/login";
+//       }
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
