789da857aba3dfc6d2e434e06b29fc3506f84cef
Riddhi-urkude <riddhi.gajananurkude2@cognizant.com>
2024-07-13 14:50:18 +0530

    Update Profile.jsx

--- src/Pages/Users/Profile.jsx
+++ src/Pages/Users/Profile.jsx
@@ -10,7 +10,6 @@ import {
   Spinner,
 } from "react-bootstrap";
 import { SideBar } from "../../Components/SideBar";
-//import { AddressAutofill } from "@mapbox/search-js-react";
 import { useFormik } from "formik";
 import { UserContext } from "../../Context/UserContext";
 import { profileSchema } from "../../utils/schema/ProfileSchema";
@@ -18,9 +17,6 @@ import { useEffect } from "react";
 import { getUserById, updateUser } from "../../Services/User.Service";
 import { toast } from "react-toastify";
 import { ImageUpload } from "../../Components/Users/ImageUpload";
-
-
-
 import axios from "axios";
 
 
@@ -41,26 +37,30 @@ const Profile = () => {
   // Server side validation error
   const [serverError, setServerError] = useState(null);
 
-  // state for image file (can be a string or file object)
-  const [image, setImage] = useState(null);
+ // state for address
+  const [addresses, setAddresses] = useState("");
 
-  // state for address
-  const [address, setAddress] = useState("");
+  const [shippingAddress, setShippingAddress] = useState("");
+
+  const [editingAddress, setEditingAddress] = useState(null);
+
+  const [showModal, setShowModal] = useState(false);
+
 
   // methods for handling address changes
-  const handleAddressInputChange = (event) => {
-    const inputValue = event.target.value;
-    setAddress(inputValue);
-    setFieldValue("address", inputValue);
-  };
+  // const handleAddressInputChange = (event) => {
+  //   const inputValue = event.target.value;
+  //   setAddress(inputValue);
+  //   setFieldValue("address", inputValue);
+  // };
 
-  // handle address input blur
-  const handleAddressInputBlur = (event) => {
-    const inputValue = event.target.value;
-    setAddress(inputValue);
-    setFieldTouched("address", true);
-    setFieldValue("address", inputValue);
-  };
+  // // handle address input blur
+  // const handleAddressInputBlur = (event) => {
+  //   const inputValue = event.target.value;
+  //   setAddress(inputValue);
+  //   setFieldTouched("address", true);
+  //   setFieldValue("address", inputValue);
+  // };
 
   // methods for sidebar
   const handleClose = () => setShow(false);
@@ -77,51 +77,110 @@ const Profile = () => {
 
       // get user data from database
       getUserById(userId)
-        .then((res) => {
+         .then((userRes) => {
             // console.log("in profile page");
-            // console.log(res);
-          setUser(res);
+            console.log(userRes);
+          setUser(userRes);
+          setAddresses(userRes.address);
 
           // setting the values of the form
           setValues({
             firstName: res.firstName,
             lastName: res.lastName,
-            state: res.state == null ? "" : res.state,
-            address: res.address == null ? "" : res.address,
-            city: res.city == null ? "" : res.city,
-            street: res.street == null ? "" : res.street,
-            pinCode: res.pinCode == null ? "" : res.pinCode,
+            phoneNumber: userRes.phoneNumber,
+            state: "",
+            address: "",
+            city: "",
+            pinCode:"",
           });
 
           // setting the values of the address in useState
-          res.address == null ? setAddress("") : setAddress(res.address);
+          //res.address == null ? setAddress("") : setAddress(res.address);
 
           // get the user's image if it exists for user otherwise set default image
-          if (res.image != null) {
-            setImage(res.image);
-          } else {
-            // get default image in case user does not have an image
-            axios
-              .get("../Asset/user-default.png", { responseType: "blob" })
-              .then((response) => {
-                const blob = response.data;
-                const file = new File([blob], "default.png", {
-                  type: "image/png",
-                });
-                setImage(file);
-              })
-              .catch(() => {
-                toast.error("Something went wrong! unable to load image");
-              });
-          }
-        })
-        .catch((err) => {
-          toast.error("Something went wrong! Please try again later");
+          // if (res.image != null) {
+          //   setImage(res.image);
+          // } else {
+          //   // get default image in case user does not have an image
+          //   axios
+          //     .get("../Asset/user-default.png", { responseType: "blob" })
+          //     .then((response) => {
+          //       const blob = response.data;
+          //       const file = new File([blob], "default.png", {
+          //         type: "image/png",
+          //       });
+          //       setImage(file);
+          //     })
+          //     .catch(() => {
+          //       toast.error("Something went wrong! unable to load image");
+        //       });
+        //   }
+        // })
+        // .catch((err) => {
+        //   toast.error("Something went wrong! Please try again later");
         });
     }
   };
 
-  // Formik form
+  const handleEditAddress = (address) => {
+    console.log("Editing Address: ", address);
+    setEditingAddress(address);
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setEditingAddress(null);
+    setShowModal(false);
+  };
+
+  const handleModalSave = async (values) => {
+    try {
+    console.log("saved values: ", values);
+    const updateAddress = { ...editingAddress, ...values };
+    const updateAddresses = addresses.map((addr) =>
+      addr._id === editingAddress._id ? { ...addr, ...values } : addr
+    );
+    setAddresses(updateAddresses);
+    await updateUserAddresses(updateAddresses);
+    setShowModal(false);
+    toast.success("Address updated successfully");
+
+    // Update the form values to reflect the save address
+    setValues({
+      ...values,
+      phoneNumber: updateAddress.phoneNumber,
+      address: updateAddress.address,
+      city: updateAddress.city,
+      state: updateAddress.state,
+      pinCode: updateAddress.pinCode
+    });
+  } catch (error) {
+    console.error("Error updating address:", error);
+    toast.error("Failed to update address, Please try again later.")
+  }
+  }; 
+  
+  const updateUserAddresses = async (updateAddresses) => {
+    const data = {
+      ...user,
+      address: updateAddresses,
+    };
+    await updateUser(userContext.userData.userId, data);
+  };
+
+  const handleSaveAddressSelect = (address) => {
+    setShippingAddress(`${address.address}`);
+    setValues({
+      ...values,
+      shippingAddress: `${address.address}`,
+      city: address.city,
+      state: address.state,
+      pinCode: address.pinCode,
+      phoneNumber: address.phoneNumber
+    });
+  };
+
+  // Formik form for profile details
   const {
     handleSubmit,
     handleChange,
@@ -136,12 +195,12 @@ const Profile = () => {
     initialValues: {
       firstName: "",
       lastName: "",
-      state: "",
-      address: "",
-      city: "",
-      street: "",
-      pinCode: "",
-      image: "",
+      // state: "",
+      // address: "",
+      // city: "",
+      // street: "",
+      // pinCode: "",
+      // image: "",
     },
     validationSchema: profileSchema,
     onSubmit: (values) => {
@@ -156,26 +215,52 @@ const Profile = () => {
       };
       console.log(data);
       // update user data in database
-      setLoading(true);
-      updateUser(userContext.userData.userId, data)
-        .then((res) => {
-          // show success toast
-          toast.success("Profile updated successfully");
-          setServerError(null);
-        })
-        .catch((err) => {
-          if (err?.response?.data?.errors) {
-            setServerError(err.response.data.errors);
-            window.scrollTo(0, 0); // scroll to top of page
-          } else {
-            toast.error("Something went wrong! Please try again later");
-          }
-        })
-        .finally(() => {
-          setLoading(false);
-        });
+      // setLoading(true);
+      // updateUser(userContext.userData.userId, data)
+      //   .then((res) => {
+      //     // show success toast
+      //     toast.success("Profile updated successfully");
+      //     setServerError(null);
+      //   })
+      //   .catch((err) => {
+      //     if (err?.response?.data?.errors) {
+      //       setServerError(err.response.data.errors);
+      //       window.scrollTo(0, 0); // scroll to top of page
+      //     } else {
+      //       toast.error("Something went wrong! Please try again later");
+      //     }
+      //   })
+      //   .finally(() => {
+      //     setLoading(false);
+      //  });
     },
  });
+
+// formik form for editing address
+ const addressFormik = useFormik({
+  initialValues: {
+    phoneNumber: "",
+    address: "",
+    city: "",
+    state: "",
+    pinCode: "",
+  },
+  onSubmit: (values) => {
+    handleModalSave(values);
+  },
+ });
+
+ useEffect(() => {
+  if (editingAddress) {
+    addressFormik.setValues({
+      phoneNumber: editingAddress.phoneNumber,
+      address: editingAddress.address,
+      city: editingAddress.city,
+      state: editingAddress.state,
+      pinCode: editingAddress.pinCode,
+    });
+  }
+ }, [editingAddress]);
 
   return (
     <>
@@ -282,7 +367,6 @@ const Profile = () => {
                   <Form.Group
                     as={Col}
                     controlId="email"
-                    md={6}
                     className="mb-3"
                   >
                     <Form.Label>Email</Form.Label>
@@ -293,120 +377,9 @@ const Profile = () => {
                       {user.email}
                     </p>
                   </Form.Group>
-                  <Form.Group
-                    as={Col}
-                    controlId="state"
-                    md={6}
-                    className="mb-3"
-                  >
-                    <Form.Label>State</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="State"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.state}
-                      isInvalid={touched.state && !!errors.state}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      {errors.state}
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </Row>
-                {/* Adding mapbox address autofill */}
-                {/* <AddressAutofill
-                  accessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-                  options={{
-                    country: "CA",
-                    language: "en",
-                  }}
-                > */}
-                  <Row>
-                    <Form.Group
-                      as={Col}
-                      controlId="address"
-                      md={12}
-                      className="mb-3"
-                    >
-                      <Form.Label>Address</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Address"
-                        autoComplete="address-line-1"
-                        value={address}
-                        onChange={handleAddressInputChange}
-                        onBlur={handleAddressInputBlur}
-                        isInvalid={touched.address && !!errors.address}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.address}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                  </Row>
-                  <Row>
-                    <Form.Group
-                      as={Col}
-                      controlId="city"
-                      md={4}
-                      className="mb-3"
-                    >
-                      <Form.Label>City</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="City"
-                        autoComplete="address-level2"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.city}
-                        isInvalid={touched.city && !!errors.city}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.city}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group
-                      as={Col}
-                      controlId="street"
-                      md={4}
-                      className="mb-3"
-                    >
-                      <Form.Label>Street</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="Street"
-                        autoComplete="address-level1"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.street}
-                        isInvalid={touched.street && !!errors.street}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.street}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group
-                      as={Col}
-                      controlId="pinCode"
-                      md={4}
-                      className="mb-3"
-                    >
-                      <Form.Label>PinCode</Form.Label>
-                      <Form.Control
-                        type="text"
-                        placeholder="PinCode"
-                        autoComplete="pincode"
-                        value={values.pinCode}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        isInvalid={touched.pinCode && !!errors.pinCode}
-                      />
-                      <Form.Control.Feedback type="invalid">
-                        {errors.pinCode}
-                      </Form.Control.Feedback>
-                    </Form.Group>
-                  </Row>
-                {/* </AddressAutofill> */}
-                <Button
+
+                  
+{/*                 <Button
                   variant="primary"
                   className="mb-3"
                   type="submit"
@@ -421,12 +394,131 @@ const Profile = () => {
                     hidden={!loading}
                   ></Spinner>
                   <span>Save</span>
-                </Button>
+                </Button> */}
+
+                  <hr />
+
+                  <Row>
+                  <Form.Group controlId="savedAddresses" className="mb-3">
+                    <Form.Label>
+                      <h3>Delivery Address</h3>
+                    </Form.Label>
+                    <div>
+                      {Array.isArray(addresses) && addresses.length > 0 ? (
+                        addresses.map((address, index) => (
+                          <Card key={index} className="mb-3">
+                            <Card.Body>
+                              <Card.Text>
+                                {address.phoneNumber}, {address.address}, {address.city}, {address.state}, {address.pinCode}
+                              </Card.Text>
+                              <Button
+                                variant="secondary"
+                                onClick={() => handleEditAddress(address)}
+                               >
+                                  Edit
+                                </Button> 
+                            </Card.Body>
+                          </Card>
+                        ))
+                      ) : (
+                        <p>No saved addresses found</p>
+                      )}
+                    </div>
+                  </Form.Group>
+                </Row>
               </Form>
             </>
           )}
         </Container>
       </Container>
+
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Address</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form noValidate onSubmit={addressFormik.handleSubmit}>
+          <Form.Group controlId="phoneNumber" className="mb-3">
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Phone Number"
+                onChange={addressFormik.handleChange}
+                onBlur={addressFormik.handleBlur}
+                value={addressFormik.values.phoneNumber}
+                isInvalid={addressFormik.touched.phoneNumber && !!addressFormik.errors.phoneNumber}
+              />
+              <Form.Control.Feedback type="invalid">
+                {addressFormik.errors.phoneNumber}
+              </Form.Control.Feedback> 
+            </Form.Group>
+
+            <Form.Group controlId="address" className="mb-3">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Address"
+                onChange={addressFormik.handleChange}
+                onBlur={addressFormik.handleBlur}
+                value={addressFormik.values.address}
+                isInvalid={addressFormik.touched.address && !!addressFormik.errors.address}
+              />
+              <Form.Control.Feedback type="invalid">
+                {addressFormik.errors.address}
+              </Form.Control.Feedback> 
+            </Form.Group>
+
+            <Form.Group controlId="city" className="mb-3">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="City"
+                onChange={addressFormik.handleChange}
+                onBlur={addressFormik.handleBlur}
+                value={addressFormik.values.city}
+                isInvalid={addressFormik.touched.city && !!addressFormik.errors.city}
+              />
+              <Form.Control.Feedback type="invalid">
+                {addressFormik.errors.city}
+              </Form.Control.Feedback> 
+            </Form.Group>
+
+            <Form.Group controlId="state" className="mb-3">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="State"
+                onChange={addressFormik.handleChange}
+                onBlur={addressFormik.handleBlur}
+                value={addressFormik.values.state}
+                isInvalid={addressFormik.touched.state && !!addressFormik.errors.state}
+              />
+              <Form.Control.Feedback type="invalid">
+                {addressFormik.errors.state}
+              </Form.Control.Feedback> 
+            </Form.Group>
+
+            <Form.Group controlId="pinCode" className="mb-3">
+              <Form.Label>Pin Code</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Pin Code"
+                onChange={addressFormik.handleChange}
+                onBlur={addressFormik.handleBlur}
+                value={addressFormik.values.pinCode}
+                isInvalid={addressFormik.touched.pinCode && !!addressFormik.errors.pinCode}
+              />
+              <Form.Control.Feedback type="invalid">
+                {addressFormik.errors.pinCode}
+              </Form.Control.Feedback> 
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Save
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
