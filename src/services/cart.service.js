a96d2d4d9e8e3ce5bd01d88ad69a01aac420142c
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-25 13:19:03 +0530

    Updated cart.service.js

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -4,54 +4,31 @@ import { publicAxiosInstance, privateAxiosInstance } from "./Axios.Service";
 const cart = {};
 
 export const getCartByUserId = async (userId) => {
-   // const res = await axios.get(`http://localhost:8080/carts/user/${userId}`);
-   // console.log("in cart service");
-   // console.log(res);
   try{
-   const res = await publicAxiosInstance.get(`/carts/user/${userId}`);
-   //console.log(res.data);
+    const res = await privateAxiosInstance.post(`/carts/user/${userId}`);
     return res.data;
   }catch(err){   
-     
+     return err;
   }
- //  return cart;
 };
   
   // add item to cart
 export const addItemToCart = async (data, userId) => {
-
-  //  const res = await axios.post(`http://localhost:8080/carts/${userId}`, data);
-
-// const res = await axios.post(`http://localhost:8080/carts/${userId}`, data,
-//  {
-//   headers: {
-//     "Authorization" : `Bearer ${token.accessToken}`,
-//     },
-//   }
-// ); 
-console.log("in add item to cart");
-  const res = await privateAxiosInstance.post(`/carts/${userId}`,data);
-
-    console.log(res);
+  try{
+    const res = await privateAxiosInstance.post(`/carts/${userId}`,data);
     return res.data;
+  }catch(err){
+    return err;
+  }
 };
   
   // remove item from cart
 export const removeItemFromCart = async (userId, itemId) => {
     try{
-//      const res = await axios.delete(`http://localhost:8080/carts/${userId}/item/${itemId}`);
       const res = await privateAxiosInstance.delete(`/carts/${userId}/item/${itemId}`);
-    //return res.data;
-    //console.log(res);
-    return res.data;
+      return res.data;
     }catch(err){
-      
+      return err;
     }
 };
   
-  // remove all items from cart
-export const removeAllItemsFromCart = async (userId) => {
-    
-    //return res.data;
-    return cart;
-};
