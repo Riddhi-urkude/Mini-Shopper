d8711d2ab3cc4d535bf94aac81b82cb541991a47
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-25 14:55:44 +0530

    Updated OrderStatusSchema.js

--- src/utils/schema/OrderStatusSchema.js
+++ src/utils/schema/OrderStatusSchema.js
@@ -3,19 +3,8 @@ import { REGEX_VALIDATIONS } from "../regex";
 
 export const OrderStatusSchema = Yup.object().shape({
 
-
-    orderStatus: Yup.string()
-   .required("Order Status is required"),
-  
-    
+    orderStatus: Yup.string().required("Order Status is required"),    
     deliveredDate: Yup.string().required("Please give an expected delivery date"),
-  
-   // orderStatus: Yup.string().required("Please select a Status"),
-  
     comments: Yup.string().required("Please put some comments for this order")
-//     reason: Yup.string()
-//   .min(10, "reason should contain 10 Numbers")
-//   .max(10, "Reason should not contain more than 450 Numbers")
-//   .required("Please provide a reason"),
 
 });
