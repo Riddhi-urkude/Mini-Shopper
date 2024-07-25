e87904e6e9f1952cda6f2a97108b96521ffedad2
Balaji-2180 <balaji.a8@cognizant.com>
2024-07-25 14:49:09 +0530

    Updated LoginSchema.js

--- src/utils/schema/LoginSchema.js
+++ src/utils/schema/LoginSchema.js
@@ -1,7 +1,15 @@
 import * as Yup from "yup";
+import { REGEX_VALIDATIONS } from "../regex";
 
 export const loginSchema = Yup.object().shape({
     role: Yup.string().required("Please provide role"),
-    userId: Yup.string().required("Please provide an email"),
-    password: Yup.string().required("Please provide a password")
+    userId: Yup.string().required("Please provide your email"),
+    password: Yup.string()
+    .min(10, "Password should be atleast 10 characacters")
+    .matches(
+      REGEX_VALIDATIONS.PASSWORD,
+      "Password should contain atleast one uppercase, one lowercase, one number and one special character"
+    )
+    .required("Please provide your password"),
+   
 });
